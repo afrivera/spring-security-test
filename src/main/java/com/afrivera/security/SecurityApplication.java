be641b5bfb3c173e1dcf053c78f34398771e0bae
Andres Rivera <andresrfelip@hotmail.com>
2024-03-30 21:08:34 -0500

    add users, permissions and roles to database with init commanLinerunner

--- src/main/java/com/afrivera/security/SecurityApplication.java
+++ src/main/java/com/afrivera/security/SecurityApplication.java
@@ -1,7 +1,17 @@
 package com.afrivera.security;
 
+import com.afrivera.security.persistence.entity.PermissionEntity;
+import com.afrivera.security.persistence.entity.RoleEntity;
+import com.afrivera.security.persistence.entity.RoleEnum;
+import com.afrivera.security.persistence.entity.UserEntity;
+import com.afrivera.security.service.impl.UserServiceImpl;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 public class SecurityApplication {
@@ -10,4 +20,93 @@ public class SecurityApplication {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(UserServiceImpl userService){
+		return args -> {
+			/* Create PERMISSIONS */
+			PermissionEntity createPermission = PermissionEntity.builder()
+					.name("CREATE")
+					.build();
+
+			PermissionEntity readPermission = PermissionEntity.builder()
+					.name("READ")
+					.build();
+
+			PermissionEntity updatePermission = PermissionEntity.builder()
+					.name("UPDATE")
+					.build();
+
+			PermissionEntity deletePermission = PermissionEntity.builder()
+					.name("DELETE")
+					.build();
+
+			PermissionEntity refactorPermission = PermissionEntity.builder()
+					.name("REFACTOR")
+					.build();
+
+			/* Create ROLES */
+			RoleEntity roleAdmin = RoleEntity.builder()
+					.roleEnum(RoleEnum.ADMIN)
+					.permissions(Set.of(createPermission, readPermission, updatePermission, deletePermission))
+					.build();
+
+			RoleEntity roleUser = RoleEntity.builder()
+					.roleEnum(RoleEnum.USER)
+					.permissions(Set.of(createPermission, readPermission))
+					.build();
+
+			RoleEntity roleInvited = RoleEntity.builder()
+					.roleEnum(RoleEnum.INVITED)
+					.permissions(Set.of(readPermission))
+					.build();
+
+			RoleEntity roleDeveloper = RoleEntity.builder()
+					.roleEnum(RoleEnum.DEVELOPER)
+					.permissions(Set.of(createPermission, readPermission, updatePermission, deletePermission, refactorPermission))
+					.build();
+			/* CREATE USERS */
+			UserEntity userSantiago = UserEntity.builder()
+					.username("santiago")
+					.password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
+					.isEnabled(true)
+					.accountNoExpired(true)
+					.accountNoLocked(true)
+					.credentialsNoExpired(true)
+					.roles(Set.of(roleAdmin))
+					.build();
+
+			UserEntity userDaniel = UserEntity.builder()
+					.username("daniel")
+					.password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
+					.isEnabled(true)
+					.accountNoExpired(true)
+					.accountNoLocked(true)
+					.credentialsNoExpired(true)
+					.roles(Set.of(roleUser))
+					.build();
+
+			UserEntity userAndrea = UserEntity.builder()
+					.username("andrea")
+					.password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
+					.isEnabled(true)
+					.accountNoExpired(true)
+					.accountNoLocked(true)
+					.credentialsNoExpired(true)
+					.roles(Set.of(roleInvited))
+					.build();
+
+			UserEntity userAnyi = UserEntity.builder()
+					.username("anyi")
+					.password("$2a$10$cMY29RPYoIHMJSuwRfoD3eQxU1J5Rww4VnNOUOAEPqCBshkNfrEf6")
+					.isEnabled(true)
+					.accountNoExpired(true)
+					.accountNoLocked(true)
+					.credentialsNoExpired(true)
+					.roles(Set.of(roleDeveloper))
+					.build();
+
+			userService.saveInitUsers(List.of(userSantiago, userDaniel, userAndrea, userAnyi));
+
+		};
+	}
 }
